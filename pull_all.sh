ca5e0df3a7f6d8d42d118ca548c7001954153607
blueburningcoder <f.karg10@gmail.com>
2015-10-28 16:41:05 +0100

    Now it gets stashed whilst the pull is --rebased

--- pull_all.sh
+++ pull_all.sh
@@ -1,5 +1,5 @@
 find ~/Coding/ -type d -name .git \
 | xargs -n 1 dirname \
 | sort \
-| while read line; do echo $line && cd $line && git pull; done
+| while read line; do echo $line && cd $line && git stash && git pull --rebase && git stash apply; done
 
