cc67003a8cedc25b6d92e504cb7ba7cfbd6afd5a
blueburningcoder <f.karg10@gmail.com>
2015-11-03 10:42:47 +0100

    now being more seperative

--- pull_all.sh
+++ pull_all.sh
@@ -1,5 +1,6 @@
 find ~/Coding/ -type d -name .git \
 | xargs -n 1 dirname \
 | sort \
-| while read line; do echo $line && cd $line && git stash && git pull --rebase && git stash apply; done
+| while read line; do echo ' ' && echo "---- ----  --- -- Next -- ---  ---- ----" && echo ' ' &&
+    echo $line && cd $line && git stash && git pull --rebase && git stash apply; done
 
