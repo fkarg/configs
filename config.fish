519a4098cf14549e6dff05f99c94bd55ce623de7
blueburningcoder <f.karg10@gmail.com>
2017-03-12 18:47:33 +0100

    Updated after some time again

---
+++ config.fish
@@ -0,0 +1,44 @@
+
+function on_exit --on-process %self
+  echo "fish is now exiting"
+end
+
+function fish_greeting
+#  echo "If you can't do it, who will?"
+#  set ran random
+  fortune -as | cowthink
+  echo
+end
+
+if status --is-interactive
+  echo "Think about reading a page or more today!"
+end
+
+
+
+# setting the path ... annoying
+for i in ~/.cabal/bin ~/.local/bin /opt/cabal/1.20/bin /opt/ghc/*/bin /opt/ghc/bin ~/.local/bin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games/ /usr/lib/postgresql/9.3/bin ~/.nvm ~/Documents/scripts+configs/scripts ~/.local/bin
+  if not contains $i $PATH
+    set -x PATH $i $PATH
+  end
+end
+# maybe additional places: /usr/local/sbin
+
+env eval "(!stack --bash-completion-script stack)"
+
+set NVM_DIR ~/.nvm
+if test -s "$NVM_DIR/nvm.sh"
+  bash . "$NVM_DIR/nvm.sh"
+end
+
+
+# set -gx PATH "~/.local/bin:~/.cabal/bin:~/.local/bin:/opt/cabal/1.20/bin:/opt/ghc/*/bin:/opt/ghc/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games/"
+set -x PATH ~/.local/bin $PATH
+
+
+# set for factis-environment
+# set -x way f
+# set -x DOCKER_HOST tcp://127.0.0.1:2375
+# 
+
+# transset --id $WINDOWID >/dev/null
