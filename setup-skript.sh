ba9eb3c252bc5b5b98aca0a5140cbb24055d7fdd
fkarg <f.karg10@gmail.com>
2019-07-17 21:39:37 +0200

    added tux config

---
+++ setup-skript.sh
@@ -0,0 +1,24 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install $(easy-reinstall-needed.txt)
+
+# haskell-install
+curl -sSL https://get.haskellstack.org/ | sh
+stack setup
+
+# rust-install
+curl https://sh.rustup.rs -sSf | sh
+
+# download uni-stuff
+cd ~/Coding
+git clone https://github.com/blueburningcoder/uni-stuff
+
+
+
+
+# move config files correctly
+# set up auto-upstream changes
+# cronjob for updating ~/Coding (with pull-all.sh)
+
+
+reboot -P now
