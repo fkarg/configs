d8b5f0de9c281a157ea87e15ac2d9b932ae95abe
fkarg <f.karg10@gmail.com>
2020-01-13 12:35:14 +0000

    some testing

---
+++ status.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# STATUS="$(timeout 1 curl -sSf http://club.entropia.de/ | rg '(geschlossen|offen)')"
+STATUS="$(timeout 1 curl -sSf http://club.entropia.de/status.json | jq -r '.club_offen')"
+
+echo $STATUS
+
+
