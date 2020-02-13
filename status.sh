0f759315a89b4747876eb88fd51ef0b89caab2de
fkarg <f.karg10@gmail.com>
2020-02-13 19:25:53 +0100

    finally fixed it

--- status.sh
+++ status.sh
@@ -3,6 +3,6 @@
 # STATUS="$(timeout 1 curl -sSf http://club.entropia.de/ | rg '(geschlossen|offen)')"
 STATUS="$(timeout 1 curl -sSf http://club.entropia.de/status.json | jq -r '.club_offen')"
 
-echo $STATUS
+echo "$STATUS"
 
 
