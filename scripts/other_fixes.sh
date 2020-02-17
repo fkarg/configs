3b251e0671790a79aeaa86be52d18aa13c692aee
fkarg <f.karg10@gmail.com>
2020-02-17 21:57:32 +0100

    adapted to previous changes

--- scripts/other_fixes.sh
+++ scripts/other_fixes.sh
@@ -5,7 +5,7 @@
 xset m 10/1 1
 dolphin4 /media/ced/HDD/ &
 dolphin4 /media/ced/HDD3/ &
-feh --no-xinerama --bg-scale /home/ced/widescreen_4.jpeg
+feh --no-xinerama --bg-scale /datadisk/Backup/sdd/ced/Pictures/Space/IMAG7672.JPG
 
 xset -dpms
 xset s off
---
+++ scripts/useful.sh
@@ -0,0 +1,5 @@
+# compress pdf files considerably
+gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/ebook -sOutputFile=out2.pdf out.pdf
+
+
+
