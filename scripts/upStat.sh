b0ea392256701f179f54686df18a1bfc75346c3e
fkarg <f.karg10@gmail.com>
2020-03-06 01:25:29 +0100

    added commonly executed script

---
+++ scripts/upStat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mount /dev/sda1 /datadisk
+feh --no-xinerama --bg-scale /home/pars/Pictures/Space/IMAG7297.JPG
+
+swapoff -a
+
+redshift -l 48.455467:11.329369 &
+xcompmgr &
+
+xset -dpms
+xset s off
+
+htop
+shutdown -P 1
+
