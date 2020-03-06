b1e9b53af9bd4c25ebf081833caeb193cd4de166
fkarg <f.karg10@gmail.com>
2020-03-06 01:28:16 +0100

    added another useful bash line

--- scripts/useful.sh
+++ scripts/useful.sh
@@ -1,5 +1,6 @@
 # compress pdf files considerably
-gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/ebook -sOutputFile=out2.pdf out.pdf
+# gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dPDFSETTINGS=/ebook -sOutputFile=out2.pdf out.pdf
 
-
+# git ignore further changes in <filename>
+# git update-index --skip-worktree <filename>
 
