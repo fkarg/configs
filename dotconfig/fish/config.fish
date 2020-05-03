3d457357da7757dce98e847fba9ce246b40c9f04
Felix <felix@delibooth.com>
2020-05-03 16:45:15 +0200

    added fish prompt

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -25,7 +25,7 @@ set -x PATH ~/.cargo/bin $PATH
 # set for factis-environment
 # set -x way f
 # set -x DOCKER_HOST tcp://127.0.0.1:2375
-# 
+#
 
 # transset --id $WINDOWID >/dev/null
 
@@ -49,3 +49,32 @@ function br
     rm -f "$f"
     eval "$d"
 end
+
+# # defined in /usr/share/fish/functions/fish_prompt.fish @ line 4
+# function fish_prompt --description 'write out the prompt'
+#     set -l last_pipestatus $pipestatus
+#     set -l normal (set_color normal)
+#
+#   # color the prompt differently when we're root
+#     set -l color_cwd $fish_color_cwd
+#     set -l prefix
+#     set -l suffix '>'
+#     if contains -- $user root toor
+#         if set -q fish_color_cwd_root
+#             set color_cwd $fish_color_cwd_root
+#         end
+#         set suffix '#'
+#     end
+#
+#   # if we're running via ssh, change the host color.
+#     set -l color_host $fish_color_host
+#     if set -q ssh_tty
+#         set color_host $fish_color_host_remote
+#     end
+#
+#   # write pipestatus
+#     set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+#
+#   echo -n -s (set_color $fish_color_user) "$user" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
+# end
+
