ab40915bee8f2f29b9c4370b07cb74bf279fac94
Felix <felix@delibooth.com>
2020-05-09 15:59:22 +0200

    update for new version of broot

--- scripts/b.sh
+++ scripts/b.sh
@@ -2,6 +2,10 @@
 
 export EDITOR=nvim
 
-source ~/.config/broot/launcher/bash/br
+P1=~/.config/broot/launcher/bash/br
+P2=~/.local/share/broot/launcher/bash/1
+[[ -f "$P1" ]] && source "$P1"
+[[ -f "$P2" ]] && source "$P2"
+
 cd ~/text_zeug/$1
 br
