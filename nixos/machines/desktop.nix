f8e4fb19d9905397414d9a13fc6b4e95fdf40d08
fkarg <f.karg10@gmail.com>
2020-07-24 20:48:11 +0200

    working desktop config

--- nixos/machines/desktop.nix
+++ nixos/machines/desktop.nix
@@ -9,11 +9,36 @@
 # - second screen config
 { config, pkgs, ... }:
 {
-  boot.loader.grub.device = ???
+  boot.loader.grub.device = "/dev/sdc"; # or "nodev" for efi only
 
+  fileSystems."/HDD3/" = 
+    { device = "/dev/disk/by-uuid/304e2387-e801-465d-b75e-3c54ba33b814";
+      fsType = "ext4";
+    };
+
+  fileSystems."/HDD/" = 
+    { device = "/dev/disk/by-uuid/adde52d0-a40b-4889-973a-7d2b032d9052";
+      fsType = "ext4";
+    };
+
+    # steam
+  environment.systemPackages = with pkgs; [
+    # (steam.override { extraPkgs = pkgs: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; nativeOnly = true; }).run
+    (steam.override { extraPkgs = pkgs: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; }).run
+  ];
+
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
+
+    # steam end
   networking.hostName = "home";
 
-  networking.interfaces.
-  networking.interfaces.
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  networking.interfaces.enp2s0.useDHCP = true;
+  networking.interfaces.wlp0s18f2u1u1.useDHCP = true;
+  networking.interfaces.wlp0s18f2u1u4.useDHCP = true;
 }
 
