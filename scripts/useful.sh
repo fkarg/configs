9e7e98b4d2be81a13fe7b7e375d0d65fd8b4305b
Felix <felix@delibooth.com>
2020-09-22 17:13:48 +0200

    added information on how to add missing upstream firmware

--- scripts/useful.sh
+++ scripts/useful.sh
@@ -8,3 +8,10 @@
 
 # jpgs to pdf
 # convert /path/to/images/*.jpg new.pdf
+
+# warnings of the type 
+# W: Possible missing firmware /lib/firmware/<module>/<driver> for module <module>
+# can be fixed by:
+# # cd /lib/firmware/<module>
+# # wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/<module>/<driver>
+# # update-initramfs -u # update
