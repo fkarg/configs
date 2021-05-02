f49f5833bc430d40c14df377ddfaddc4c4f3c4c6
fkarg <f.karg10@gmail.com>
2021-05-02 14:06:29 +0200

    slightly expanded patchelf script

--- scripts/patchelf_fix.sh
+++ scripts/patchelf_fix.sh
@@ -1,5 +1,10 @@
-for exe in ~/.conda/bin/*
+
+FIX=~/.conda/bin/*
+# FIX=~/.conda/envs/*/bin/*
+
+for exe in $FIX
 do
+    echo [$exe]
     patchelf --print-interpreter $exe
-    # patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $exe
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $exe
 done
