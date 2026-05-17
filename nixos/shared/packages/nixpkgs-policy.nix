4e6291a1639d36c14cda9954a7e59f32d7ac6f1c
Felix Karg <f.karg10@gmail.com>
2026-05-17 14:49:52 +0200

    Add nixos-unstable overlay for vscode and modules closure workarounds

--- nixos/shared/packages/nixpkgs-policy.nix
+++ nixos/shared/packages/nixpkgs-policy.nix
@@ -1,6 +1,46 @@
-{ ... }:
+{ lib, ... }:
 
 {
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.pulseaudio = true;
+
+  # Temporary nixos-unstable workarounds for currently broken package/build
+  # paths pulled into this desktop configuration.
+  nixpkgs.overlays = [
+    (final: prev: {
+      vscode = prev.vscode.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.jq.bin ];
+      });
+
+      makeModulesClosure =
+        {
+          kernel,
+          firmware,
+          rootModules,
+          allowMissing ? false,
+          extraFirmwarePaths ? [ ],
+        }:
+        final.stdenvNoCC.mkDerivation {
+          name = kernel.name + "-shrunk";
+          builder = final.writeShellScript "modules-closure-builder" ''
+            export PATH=${lib.makeBinPath [
+              final.coreutils
+              final.gnugrep
+              final.gnused
+              final.kmod
+              final.nukeReferences
+            ]}:$PATH
+            exec ${final.bash}/bin/bash ${prev.path}/pkgs/build-support/kernel/modules-closure.sh
+          '';
+          inherit
+            kernel
+            firmware
+            rootModules
+            allowMissing
+            extraFirmwarePaths
+            ;
+          allowedReferences = [ "out" ];
+        };
+    })
+  ];
 }
